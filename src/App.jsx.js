36805c48a51dcdf3184bb7f09d2d7cebbc647623
Duane King <duane@kingduane.com>
2024-11-09 00:03:20 -0800

    Initial Commit

---
+++ src/App.jsx.js
@@ -0,0 +1,11 @@
+import ObjectDetection from './components/ObjectDetection'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black">
+      <ObjectDetection />
+    </div>
+  )
+}
+
+export default App
